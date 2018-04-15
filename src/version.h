b23693c4cab2f7319879fa122c73b697de58d653
depeder <rodrigo@depeder.com.br>
2018-04-15 18:36:16 -0300

    up configure.ac clientversion.h

--- src/version.h
+++ src/version.h
@@ -11,7 +11,7 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 90040;
+static const int PROTOCOL_VERSION = 90050;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 3;
